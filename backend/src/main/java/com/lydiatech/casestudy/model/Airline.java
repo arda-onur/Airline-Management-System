39dc997ecd36e0713006c1b3f7dfb4de35b1aa17
arda-onur <ardaonur15@gmail.com>
2025-11-11 14:47:15 +0300

    Implemented Airline Entity Model

--- backend/src/main/java/com/lydiatech/casestudy/model/Airline.java
+++ backend/src/main/java/com/lydiatech/casestudy/model/Airline.java
@@ -1,10 +1,8 @@
 package com.lydiatech.casestudy.model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Entity
