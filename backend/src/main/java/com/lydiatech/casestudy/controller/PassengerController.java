1e402935b89c391c4b596081fe6332d98c2c22f1
arda-onur <ardaonur15@gmail.com>
2025-11-11 14:50:23 +0300

    Implemented Passenger Controller

---
+++ backend/src/main/java/com/lydiatech/casestudy/controller/PassengerController.java
@@ -0,0 +1,44 @@
+package com.lydiatech.casestudy.controller;
+
+import com.lydiatech.casestudy.mapper.PassengerMapper;
+import com.lydiatech.casestudy.model.Passenger;
+import com.lydiatech.casestudy.request.PassengerRequest;
+import com.lydiatech.casestudy.service.PassengerService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/passenger")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:5173")
+public class PassengerController {
+
+    private final PassengerMapper passengerMapper;
+    private final PassengerService passengerService;
+
+
+    @PostMapping("/create")
+    public ResponseEntity<String> registerPassenger(@RequestBody @Valid PassengerRequest passengerRequest){
+      return ResponseEntity.status(HttpStatus.CREATED)
+              .body(this.passengerService.registerPassenger(this.passengerMapper.map(passengerRequest)));
+    }
+
+    @GetMapping("/getAllPassenger")
+    public ResponseEntity<Page<Passenger>> registerPassenger(@RequestParam int page, @RequestParam int size){
+        Pageable pageable = PageRequest.of(page, size);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(this.passengerService.getAllPassenger(pageable));
+    }
+    @GetMapping("/getRelatedFlights")
+    public ResponseEntity<Page<Passenger>> registerPassenger(@RequestParam int page, @RequestParam int size, @RequestParam String email){
+        Pageable pageable = PageRequest.of(page, size);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(this.passengerService.getAllPassenger(pageable));
+    }
+}
