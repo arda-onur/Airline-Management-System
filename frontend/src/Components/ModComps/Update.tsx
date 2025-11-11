60aead1b55afeadcd4d311747b3ee0115dfeb470
arda-onur <ardaonur15@gmail.com>
2025-11-11 21:00:40 +0300

    Implemented Under Maintenance section for Booking Update.tsx

--- frontend/src/Components/ModComps/Update.tsx
+++ frontend/src/Components/ModComps/Update.tsx
@@ -4,6 +4,19 @@ import SubmitButton from "./SubmitButton.tsx";
 
 export default function Update(props){
     const variables = Object.keys(props).filter(key => key !== "api");
+    const isUnderMaintenance = "underMaintenance" in props;
+
+    if (isUnderMaintenance) {
+        return <p style={{
+            display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "100vh",
+                fontSize: "1.5rem",
+                fontWeight: "bold"
+        }}>Update Section Under Maintenance</p>;
+    }
+
 
     function handleSubmit(e: React.FormEvent<HTMLFormElement>){
         e.preventDefault();
@@ -32,21 +45,23 @@ export default function Update(props){
     }
 
     return(
-        <div className="add">
-            <form className="add" onSubmit={handleSubmit}>
-                {variables.map(variable => (
-                    <div className="addSubComp">
-                        <label> {variable}</label>
-                        {variable === "DepartureTime" || variable === "ArrivalTime" ?
-                            <input type= "datetime-local" name={variable} /> :
-                            <input  name={variable} /> }
-                    </div>
-                ))}
 
-                {variables.length > 0 ? <SubmitButton/> : null}
 
-            </form>
+      <div className="add">
+          <form className="add" onSubmit={handleSubmit}>
+              {variables.map(variable => (
+                  <div className="addSubComp">
+                      <label> {variable}</label>
+                      {variable === "DepartureTime" || variable === "ArrivalTime" ?
+                          <input type= "datetime-local" name={variable} /> :
+                          <input  name={variable} /> }
+                  </div>
+              ))}
 
-        </div>
+              {variables.length > 0 ? <SubmitButton/> : null}
+
+          </form>
+
+      </div>
     );
 }
