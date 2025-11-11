c78952e06b57022636c3698cb98b6c959e940f57
arda-onur <ardaonur15@gmail.com>
2025-11-11 15:01:30 +0300

    Implemented Booking Component

---
+++ frontend/src/Components/mainComps/Booking.tsx
@@ -0,0 +1,35 @@
+import '../../assets/cssClass/SubCompCss/Header.css'
+import ModSelection from "../ModComps/ModSelection.tsx";
+
+export default function Booking() {
+    const add = {
+        flightId:"FlightId",
+        email:"Email",
+        seatNumber:"SeatNumber"
+    }
+
+    const api = {
+        ApiCreate: "http://localhost:8080/booking/create",
+        ApiUpdate:"http://localhost:8080/booking/update",
+        ApiCancel:"http://localhost:8080/booking/cancel",
+        ApiList:"http://localhost:8080/booking/getAllBookings"
+    } as const
+
+    const list = {
+        id: "Booking ID",
+        flightNumber: "Flight Number",
+        origin: "Origin",
+        destination: "Destination",
+        departureTime: "Departure Time",
+        arrivalTime: "Arrival Time",
+        basePrice: "BasePrice",
+        passenger: "Passenger",
+        bookingStatus: "Booking Status",
+    };
+    return(
+        <>
+            <h2 id="HeaderName">Booking Management</h2>
+            <ModSelection add={add} list={list} api={api}/>
+        </>
+    );
+}
